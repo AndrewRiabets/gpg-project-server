20f94d244cbe7bda7b87f40a4e18e7902f39bd36
AndrewRiabets <Inferion5@gmail.com>
2022-05-10 18:38:51 +0300

    make it better

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import app from "./app";
 const PORT = process.env.PORT || 5001;
 
 db.then(() => {
-  app.listen(PORT, () => {
+  app.listen(PORT, async () => {
     console.log(`Example app listening on port ${PORT}`);
   });
 }).catch((err) => {
